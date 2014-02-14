64a6a8edb3a005488ef0e53fcfd5df7020a3ee4a
Gianvito Pio <pio.gianvito@gmail.com>
2014-02-14 09:51:18 +0100

    Added EDB Status page

--- src/Kdde/EdbBundle/Controller/PeopleController.php
+++ src/Kdde/EdbBundle/Controller/PeopleController.php
@@ -8,13 +8,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class PeopleController extends Controller
 {
-    
     public function indexAction()
     {
-    	
-    	//$users = $this->getDoctrine()->getRepository('KddeEdbStoreBundle:Utente')->findAll();
-    	//print_r($users);
-    
-        return $this->render('KddeEdbBundle:People:index.html.twig');
+        return $this->render('KddeEdbBundle:InfoPages:people.html.twig');
     }
 }
