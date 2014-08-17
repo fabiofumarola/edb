51932015f3e01b1f02d7193d527aa3c91c045bb0
Gianvito Pio <pio.gianvito@gmail.com>
2014-08-17 20:47:32 +0200

    Added extension of transcription text in the results page

--- web/js/basic-result.js
+++ web/js/basic-result.js
@@ -1,3 +1,16 @@
 $('document').ready(function() {
 	$('#epiResults').tablesorter(); 
+	
+	var rowCount = $('#epiResults tr').length;
+	for(var i=1; i <=rowCount; i++)
+	{
+		var rowName = '#transcription' + i;
+//		alert(rowName);
+		$(rowName).readmore({
+			  speed: 75,
+			  maxHeight: 20
+			});
+	}
+	
 });
+
